0ff5af31c16406bd47cb96c52cfc4325fb55a942
bpetterborg <ben.petterborg@gmail.com>
2021-10-22 08:53:51 -0400

    Add thanks to RPi.GPIO2 devs

--- pi_vex_393.py
+++ pi_vex_393.py
@@ -1,6 +1,8 @@
 # 
 #	Motor control library
 #	Use to control Vex 393s with RPi.GPIO2
+#
+#	Also, thanks to RPi.GPIO2 devs, you guys are awesome!
 #	
 #	TODO:
 #		- Make a separate repo for this, put it on the PyPI
