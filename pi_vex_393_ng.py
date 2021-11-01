f7c8965484d1c244ad3457c98189f794ffa07719
bpetterborg <ben.petterborg@gmail.com>
2021-11-01 09:23:28 -0400

    Rewriting the whole thing

---
+++ pi_vex_393_ng.py
@@ -0,0 +1,24 @@
+#
+# Motor control library for Vex 393s
+# Based on RPi.GPIO2
+# 
+# Thanks to the RPi.GPIO2 devs, you guys are awesome!
+#
+#	TODO:
+#		- figure out how to set different motor numbers/names
+#
+#
+#
+#
+
+import RPi.GPIO2 as GPIO
+
+class Motors:
+
+	def __init__(self) -> None:
+		pass
+
+	def test_module(self):
+		return 'Module working probably'
+
+	
