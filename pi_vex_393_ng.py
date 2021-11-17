6163e12a382989c79ed2783d0b1c3f74e182110f
bpetterborg <ben.petterborg@gmail.com>
2021-11-16 21:48:05 -0500

    Changed stuff with how motor config is handled.

--- pi_vex_393_ng.py
+++ pi_vex_393_ng.py
@@ -14,29 +14,34 @@
 import RPi.GPIO2 as GPIO	# gpio access
 import json					# config files
 
-class GetMotorConfig:
-	
-	def __init__(self) -> None:
-		pass
-
-	def read_config():
-		json.loads(open('motors.json').read())
-
-class GetStatusConfig:
+# get configs
+class GetConfig:
 
 	def __init__(self) -> None:
 		pass
 
-	def read_config():
-		json.loads(open('status_messages.json').read())
+	def status(self, status_message):
+		json.loads(open('status_messages.jsonc').read())
 
+	def motors():
+		json.loads(open('motors.jsonc').read())
+
+# actually doing stuff with the motors.
 class Motors:
 
-	def __init__(self) -> None:
+	def __init__(self, motor) -> None:
 		pass
 
-	def test_module(self):
+	def testModule(self):
 		return 'Module working probably'
 
-	def select_motor(self, motor):
-		return f'Select motor {motor}'
+	def selectMotor(self, motor):
+		return f'Select motor {motor}' # change this to debug status json read thing
+		motor_name = GetConfig.motors()["motor1"]
+
+
+
+# unused but saving for later
+
+# just trying stuff for the multiple motor support
+#motor_count = json.loads(open('motors.jsonc').read())['motor_count']
